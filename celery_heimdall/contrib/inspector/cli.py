0bab8f4a383284d08f322a92e27dd55215cdfb38
Tyler Kennedy <tk@tkte.ch>
2022-12-13 16:45:52 -0500

    Better help strings and configurable database url.

--- celery_heimdall/contrib/inspector/cli.py
+++ celery_heimdall/contrib/inspector/cli.py
@@ -1,26 +1,20 @@
+from pathlib import Path
+
+import click
 from celery import Celery
-
-try:
-    import click
-    import quart
-except ImportError:
-    print(
-        'heimdall-inspector is not installed - install it with\n'
-        '\tpip install celery-heimdall[inspector]'
-    )
-    raise SystemExit()
-
 
 from celery_heimdall.contrib.inspector.monitor import monitor
 
 
 @click.group()
 def cli():
-    pass
+    """
+    heimdall-inspector provides tools for introspecting a live Celery cluster.
+    """
 
 
 @cli.command('monitor')
-@click.argument('broker')
+@click.argument('broker_url')
 @click.option(
     '--enable-events',
     default=False,
@@ -30,15 +24,27 @@ def cli():
         ' emitting worker events before starting the server.'
     )
 )
-def monitor_command(broker, enable_events):
+@click.option(
+    '--db',
+    default='heimdall.db',
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    help=(
+        'Use the provided path to store our sqlite database.'
+    )
+)
+def monitor_command(broker_url: str, enable_events: bool, db: Path):
     """
-    Starts the monitor to record Celery events.
+    Starts a monitor to watch for Celery events and records them to an SQLite
+    database.
+
+    Optionally enables event monitoring on a live cluster if --enable-events is
+    provided. Note that it will not stop events when finished.
     """
     if enable_events:
-        celery_app = Celery(broker=broker)
+        celery_app = Celery(broker=broker_url)
         celery_app.control.enable_events()
 
-    monitor(broker=broker)
+    monitor(broker=broker_url, db=db)
 
 
 if __name__ == '__main__':
